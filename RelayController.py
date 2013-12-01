a59bb110d93ad8752d58ca0c0d51eeb01350cb08
Ryan Brooks <ryanbrooksis@gmail.com>
2013-12-01 16:58:19 +0000

    Adding relay control

--- RelayController.py
+++ RelayController.py
@@ -1,40 +1,49 @@
+import datetime
 import wiringpi2 as wiringpi 
 import sys
 
-wiringpi.wiringPiSetupGpio()
+class RelayController:
+  """ A simple sensor class for the Ciseco Slice of Pi relay (http://shop.ciseco.co.uk/slice-of-relay/) """
+  
+  
+  name = "Relay Control: Slice of Relay: A"
+  on  = 1
+  off = 0
+  OUTPUT_MODE = 1 
+  relayA = 24
+  relayB = 25
 
-relayArg = sys.argv[1]
-stateArg = sys.argv[2]
+  wiringpi.wiringPiSetupGpio()
 
-print "Switching relay " + relayArg + " to " + ` int(stateArg)`
+  # FIXME this should be a metaprogrammed setup - many sensor instances for a single slice of pi
+  def set(self,newState):
+    
+    # FIXME need to catch error
+    print "Switching relay " + relayA + " to " + ` int(stateArg)`
 
-relayA = 24
-relayB = 25
-
-on  = 1
-off = 0
-if relayArg == 'A' or relayArg == 'a':
-	selectedRelay = relayA
-else:
-	selectedRelay = relayB
-
-if int(stateArg) == 1:
-	relayValue = 1
-else:
-	relayValue = 0 
-
-OUTPUT = 1
-wiringpi.pinMode(selectedRelay,OUTPUT)
-
-initialState = wiringpi.digitalRead(selectedRelay)
-wiringpi.digitalWrite(selectedRelay,relayValue)
-currentState = wiringpi.digitalRead(selectedRelay)
-
-if(initialState == currentState):
-	print "Target state was already set. No action taken"
-else:
-	print "Switched"
-#print "Initial state"
-#print initialState
-#print "Current state"
-#print currentState
+    if int(stateArg) == 1:
+    	relayValue = 1
+    else:
+    	relayValue = 0 
+    
+    OUTPUT = 1
+    wiringpi.pinMode(relayA,self.OUTPUT_MODE)
+    
+    initialState = wiringpi.digitalRead(relayA)
+    wiringpi.digitalWrite(relayA,newState)
+    currentState = wiringpi.digitalRead(relayA)
+    
+    result = None
+    if(initialState == currentState):
+    	result = "Target state was already set. No action taken"
+    else:
+    	result = "Switched"
+    
+    errors = None
+    
+    return {
+        "controller": self.name,
+        "timestamp": datetime.datetime.now().isoformat(' '),
+        "result": result,
+        "errors": errors,
+    }
